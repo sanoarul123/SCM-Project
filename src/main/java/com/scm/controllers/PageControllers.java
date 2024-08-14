98e0a2835355875ae1292f0005646aff18247c0f
sanoarul123 <59704648+sanoarul123@users.noreply.github.com>
2024-08-14 19:51:01 +0600

    navbar commit

--- src/main/java/com/scm/controllers/PageControllers.java
+++ src/main/java/com/scm/controllers/PageControllers.java
@@ -3,6 +3,8 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageControllers {
@@ -15,9 +17,23 @@ public class PageControllers {
         return "home";
     }
 
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("Login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+
     @RequestMapping("/about")
     public String about(Model model) {
-        // model.addAttribute("isLogin", true);
 
         System.out.println("This is about page");
 
@@ -30,4 +46,5 @@ public class PageControllers {
 
         return "services";
     }
+
 }
